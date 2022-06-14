30756a7710fb59064c38e394ba3e130ba4e4e2d8
fErhaniSNeivE <107480139+fErhaniSNeivE@users.noreply.github.com>
2022-06-14 20:22:07 +0800

    Add files via upload

---
+++ prioritas_tuan_memet.py
@@ -0,0 +1,16 @@
+prioritas_tuan_memet = { 1 : "Genshin Impact", 2 : "Keluarga", 3 : "Tuhan"}
+print(prioritas_tuan_memet)
+
+selection = input("Num > ")
+
+def sel(selection):
+    if selection == 1:
+        print("Kamu Durhaka Super JAHANAM, Genshin diatas Tuhan dan keluarga ?!?!?!?")
+    elif selection == 2:
+        print("Kamu durhaka, Keluarga diatas TUHAN ?!?!?!?")
+    elif selection == 3:
+        print("Tuhan itu nomor 1, yang lain numpang")
+    else:
+        print("Goblogggggg")
+
+sel(selection)
