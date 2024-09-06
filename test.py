2c6fc2a703af1efddfd1079590e440f9e96bda3e
nIA <107480139+neiveIsAfraid@users.noreply.github.com>
2024-09-06 21:00:32 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+#syntax
+#struktur pembahasaan dalam bahasa pemrograman
+#ini komentar satu baris
+"""
+ini komentar multi baris
+"""
+
+#Ini buat 
+#Nampilin hello world
+print("Hello World!")
+
+#variable
+nama = "Gatau siapa"
+print(nama)
+
+#TIPE DATA
+
+#ini tipe data string, string ibarat kek kalimat atau kata kata. biasanya dipakai buat nama
+"Hallo aku adalah string"
+
+#ini tipe data integer, integer isinya angka bilangan bulat atau negatif
+satu_integer = 1
+integer_dua = 5
+
+#ini tipe data float, buat bil desimal
+float_satu = 1.5
+float_dua = 12313.14513437304071200194
+
+#ini tipe data boolean, buat kondisi kalau salah(False) atau benar(True) (Depannya gede ya. True bukan true, false juga sama)
+bool_satu = True
+bool_dua = False 
