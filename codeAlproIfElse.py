d0defb774e95a0734121a2a1f755f6a5c6969fe9
nIA <107480139+neiveIsAfraid@users.noreply.github.com>
2024-09-14 20:32:07 +0800

    Add files via upload

---
+++ codeAlproIfElse.py
@@ -0,0 +1,16 @@
+ #if
+
+nilai = 95
+
+if nilai >= 90:
+    print("Selamat anda mendapat nilai A")
+    if nilai >=95:
+        print("Anda berkesempatan untuk mengajukan beasiswa program Indonesia Emas")
+elif nilai >= 80:
+    print("Anda mendapat mendapat nilai B")
+elif nilai >= 70:
+    print("Anda mendapat mendapat nilai C")
+elif nilai >= 60:
+    print("Belajar lebih giat lagi ya")
+print("Terimakasih")
+
