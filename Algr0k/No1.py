10bf4b9fb01dfdc57b2f9e6fa5e93f217b67eccb
nIA <107480139+neiveIsAfraid@users.noreply.github.com>
2024-09-17 19:47:41 +0800

    Create No1.py

---
+++ Algr0k/No1.py
@@ -0,0 +1,17 @@
+"""
+Buatlah program untuk mencetak bilangan ganjil dari N sampai dengan 1 dimana
+N adalah bilangan bulat masukkan pengguna. Sebagai contoh ketika pengguna
+memasukkan 10, maka komputer akan mencetak 9 7 5 3 1.
+"""
+
+number = int(input("Give me a number: "))
+to_iterate = number
+
+while to_iterate >= 0:
+    if to_iterate % 2 != 0:
+        print(f"The {to_iterate} is an odd number")
+    to_iterate-=1
+
+"""
+
+"""
