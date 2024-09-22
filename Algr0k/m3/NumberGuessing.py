e6cb12e9b83c6a766d3b0961c6c9c415fe8f7639
nIA <107480139+neiveIsAfraid@users.noreply.github.com>
2024-09-22 21:48:52 +0800

    Create NumberGuessing.py
    Jangan lupa commentnya dihapus dlu kalo mau buat pembahasan

---
+++ Algr0k/m3/NumberGuessing.py
@@ -0,0 +1,49 @@
+"""
+Number guessing game
+Merah
+Hijau
+Jangan lupa dihapus ketika ingin membuat pembahasan
+"""
+import sys, random
+
+nama = input("Nama: ")
+umur = int(input("Umur: "))
+
+if umur < 17:
+    sys.exit("\033[31mAnak kecil tidak boleh mainan game seperti ini.\033[0m")
+
+print(f"\n\n\nSELAMAT DATANG {nama} DI PERMAINAN TERBAIK ABAD INI\nKamu akan menebak angka yang dipilih komputer\nApakah kamu ingin bermain ?")
+
+konfirmasi = input("(y/n): ")
+if konfirmasi == "y":
+    print("Kita akan bermain!!!\nTebak satu angka dari 1 - 20")
+else:
+    i = 100
+    while i>0:
+        print("Kamu tidak seru sekali -_-")
+        i -= 1
+    sys.exit()
+
+#mulai game
+pilihan_komputer = random.randint(1, 20)
+pil = 0
+tries = 0
+while pil != pilihan_komputer:
+    
+    if tries > 5:
+        confirm = input("Apakah anda ingin menyerah ? ")
+        if confirm == "n":
+            print("Anda adalah pejuang, saya suka")
+            tries -= 5
+        else:
+            sys.exit("Anda cupu")
+    
+    pil = int(input(": "))
+    if pil > pilihan_komputer:
+        print("Turunkan sedikit...")
+        tries += 1
+    elif pil < pilihan_komputer:
+        print("naikkan sedikit...")
+        tries += 1
+    elif pil == pilihan_komputer:
+        print(f"Anda Benar !!!\n Selamat {nama} kamu berhasil memenangkan game ini!!!")
