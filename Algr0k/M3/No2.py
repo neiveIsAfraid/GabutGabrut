9318c497a102cbfce12e7c1f387b770e16e7b0dd
nIA <107480139+neiveIsAfraid@users.noreply.github.com>
2024-09-24 14:28:49 +0800

    Update No2.py

--- Algr0k/M3/No2.py
+++ Algr0k/M3/No2.py
@@ -9,53 +9,42 @@ while pilihan != 4:
         print("\n\n===Persegi===\n1.Luas\n2.Keliling")
         luas_or_keliling = int(input(": "))
         if luas_or_keliling == 1:
-            panjang = int(input("Panjang: "))
-            lebar = panjang
-            print(f"\n\033[32mLuas: {panjang * lebar} cm\033[0m\n")
+            sisi = float(input("Panjang Sisi: "))
+            print(f"\n\033[32mLuas: {sisi * sisi} cm\033[0m\n")
             lanjut = input("Tekan enter untuk lanjut\n\n")
 
         elif luas_or_keliling == 2:
-            panjang = int(input("Panjang: "))
-            lebar = panjang
-            print(f"Keliling: {2 * (panjang + lebar)}")
+            sisi = float(input("Panjang sisi: "))
+            print(f"Keliling: {4 * sisi}")
             lanjut = input("Tekan enter untuk lanjut\n\n")
 
     elif pilihan == 2:
         print("\n\n===Persegi Panjang===\n1.Luas\n2.Keliling\n")
         luas_or_keliling = int(input(": "))
         if luas_or_keliling == 1:
-            panjang = int(input("Panjang: "))
-            lebar = int(input("Lebar: ")) 
-            satuan = ""
-            print(f"Luas: {panjang * lebar} {satuan}\n")
+            panjang = float(input("Panjang: "))
+            lebar = float(input("Lebar: ")) 
+            print(f"Luas: {panjang * lebar}\n")
             lanjut = input("Tekan enter untuk lanjut\n\n")
 
         elif luas_or_keliling == 2:
-            panjang = int(input("Panjang: "))
-            lebar = int(input("Lebar: "))
-            satuan = ""
-            print(f"Keliling: {2 * (panjang + lebar)} {satuan}")
+            panjang = float(input("Panjang: "))
+            lebar = float(input("Lebar: "))
+            print(f"Keliling: {2 * (panjang + lebar)}")
             lanjut = input("Tekan enter untuk lanjut\n\n")
 
     elif pilihan == 3:
         print("3.Segitiga")
         print("\n\n===Segitiga Sama Sisi===\n1.Luas\n2.Keliling\n")
-        luas_or_keliling = int(input(": "))
+        luas_or_keliling = float(input(": "))
         if luas_or_keliling == 1:
-            panjang = int(input("Panjang: "))
-            satuan = ""
-            print(f"Luas: {(1/4 * panjang**2)* math.sqrt(3)} {satuan}\n")
+            panjang = float(input("Panjang: "))
+            print(f"Luas: {(1/4 * panjang**2)* math.sqrt(3)}\n")
             lanjut = input("Tekan enter untuk lanjut\n\n")
 
         elif luas_or_keliling == 2:
-            panjang = int(input("Panjang: "))
-            satuan = ""
-            print(f"Keliling: {3 * panjang} {satuan}")
+            panjang = float(input("Panjang: "))
+            print(f"Keliling: {3 * panjang}")
             lanjut = input("Tekan enter untuk lanjut\n\n")
     else:
         print("\033[31mNot in choice.\033[0m")
-
-"""
-Buatlah program luas dan keliling bidang persegi, persegi panjang, dan segitiga
-dengan pilihan menu. Selama menu keluar tidak dipilih, program terus berjalan.
-"""
